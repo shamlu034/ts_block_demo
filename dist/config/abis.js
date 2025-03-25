550986be3cbc8b1014befd2b4b0cbd5c4faedc30
aaronyann <aaronyann90@gmail.com>
2025-03-25 19:01:42 +0800

    delete this file

--- dist/config/abis.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.STAKING_ABI = void 0;
-exports.STAKING_ABI = [
-    'event Staked(address user, uint256 amount)',
-    'event UnStaked(address claimer, uint256 stakedTokens)',
-];
